3dff37ba766e75fd113bea10fa5cd487c42cdf1e
Nicomagno24 <nicomag2498@gmail.com>
2019-09-09 20:48:43 -0300

    funciones anonimas en los eventos y callbacks

--- ejercicios js/validar-link.js
+++ ejercicios js/validar-link.js
@@ -4,27 +4,67 @@ console.log("mensaje desde validar-link.js")
 let links = document.querySelectorAll("nav a")
 
 
-links.forEach(manipularLink) 
+// links.forEach(manipularLink) 
 
-
+/*
 function manipularLink(link){
 	console.log(link)
 
 	link.onclick = validar
 
 }
+*/
 
-
+/*
 function validar(evento){
 
 	evento.preventDefault() //frena el comportamiento predeterminado del link/hipervinculo
 
-	let rta = confirm("esta seguro que desea salir de la pagina")
+	let url = evento.target.href //accedo al atributo href del target del evento, manipulacion de estructura
+
+	let rta = confirm(`esta seguro que desea ir a ${evento.target.innerText}`)
+
 
 	if(rta){
 		alert("chau")
+		window.location.href = url
 	}else{
 
 	}
 
 }
+*/
+
+
+
+//funcion anonima 
+links.forEach( function (link){
+
+	console.log(link)
+
+	link.onclick = function (evento){
+
+		evento.preventDefault() //frena el comportamiento predeterminado del link/hipervinculo
+
+		let url = evento.target.href //accedo al atributo href del target del evento, manipulacion de estructura
+
+		let rta = confirm(`esta seguro que desea ir a ${evento.target.innerText}`)
+
+
+		if(rta){
+			alert("chau")
+			window.location.href = url
+		}else{
+
+		}
+
+	}
+
+})
+
+//link es una variable que guarda los elementos de la coleccion
+
+
+
+
+
